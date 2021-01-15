da2d92fa4096229382137ededfdaea9da2fc90a8
bw.devel <bwilsonde+github@gmail.com>
2021-01-15 23:09:10 +0100

    initial commit

---
+++ main.lua
@@ -0,0 +1,27 @@
+function love.load()
+
+
+end
+
+
+function love.update(dt)
+
+
+end
+
+
+function love.draw()
+
+
+
+end
+
+
+function love.keypressed(k)
+
+end
+
+
+function love.keyreleased(k)
+  if k == 'q' then love.event.quit() end
+end
